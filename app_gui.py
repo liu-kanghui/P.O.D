33de88a0f9782a82fe80ec6558207bfa9b76430a
Emily Wright <emily.wright005@gmail.com>
2018-03-15 21:17:20 -0700

    Add files via upload

--- app_gui.py
+++ app_gui.py
@@ -4,7 +4,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5 import uic
-from Light import Lights
+# from Light import Lights
 import detect_device_ip
 import waterpump_server
 
@@ -19,9 +19,13 @@ class MyApp(QMainWindow):
         super(MyApp, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.calc_tax_button.clicked.connect(self.CalculateTax)
-        self.ui.light_button.clicked.connect(self.TurnOnLight)
+        # Open file
+        # openFile = QtGui.QAction("&Open File", self)
+        # self.ui.open_file_line_edit.clicke]d.connect(self.file_open)
+        # self.ui.start_lights_button.clicked.connect(self.TurnOnOffLight)
+        # self.ui.stop_lights_button.clicked.connect(self.TurnOffLight)
         self.ui.start_pumping_button.clicked.connect(self.TurnOnWaterPump)
+        # self.ui.stop_pumping_button.clicked.connect(self.TurnOffWaterPump)
 
     def CalculateTax(self):
         price = int(self.ui.price_box.toPlainText())
@@ -30,19 +34,31 @@ class MyApp(QMainWindow):
         total_price_string = "The total price with tax is: " + str(total_price)
         self.ui.results_window.setText(total_price_string)
 
-    def TurnOnLight(self):
-        connectedpiZeroHostDict = detect_device_ip.arp_scan()
-        # assign all the pod to control light as hard on and off.
-        # just for now testing.
-        for name, host in connectedpiZeroHostDict.items():
-            print(host)
-            lightPattern = Lights('192.168.1.102')
-            lightPattern.hardOnOffLED(1, 1, 120)
+    # def TurnOnLight(self):
+    #     connectedpiZeroHostDict = detect_device_ip.arp_scan()
+    #     # assign all the pod to control light as hard on and off.
+    #     # just for now testing.
+    #     for name, host in connectedpiZeroHostDict.items():
+    #         print(host)
+    #         lightPattern = Lights('192.168.1.102')
+    #         lightPattern.hardOnOffLED(1, 1, 120)
+    #
+    # def TurnOffLight(self):
+
 
     def TurnOnWaterPump(self):
         run_time = (self.ui.pump_run_time.value())
         delay_time = (self.ui.pump_delay_time.value())
         waterpump_server.pump_water('192.168.1.102', run_time, delay_time)
+
+    def TurnOffWaterPump(self):
+        run_time = 0
+        delay_time = 0
+        waterpump_server.pump_water('192.168.1.102', run_time, delay_time)
+
+    # def file_open(self):
+    #     name = QtGui.QFileDialog.getOpenFileName(self, "Open File")
+    #     file = open(name, 'r')
 
 
 if __name__ == "__main__":
