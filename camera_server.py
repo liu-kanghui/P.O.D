ae662c3e20e301215682e0e0cd87f1ff7c4e9bf5
jessekline <klinej5@wwu.edu>
2018-05-14 18:31:45 -0700

    Updated camera code w/ select

--- camera_server.py
+++ camera_server.py
@@ -1,5 +1,7 @@
 import io
+import select
 import socket
+import sys
 import struct
 from PIL import Image
 from datetime import datetime
@@ -11,32 +13,57 @@ server_socket = socket.socket()
 server_socket.bind(('0.0.0.0', 8000))
 server_socket.listen(0)
 
+inputs = [server_socket]
+
 # Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
+# connection = server_socket.accept()[0].makefile('rb')
+
+# Server setup to allow client PODs to connect
+# socket_dic = {}
+# socket_dic[server_socket]
+
 try:
+
     while True:
+
+        readable, writeable, exceptions = select.select(inputs, [], [])
+
+        for s in readable:
+
+            if s is server_socket:
+
+                connection = server_socket.accept()[0].makefile('rb')
+                # connection.setblocking(0)
+                # inputs.append(connection)
+
         # Read the length of the image as a 32-bit unsigned int. If the
         # length is zero, quit the loop
-        image_len = struct.unpack('<L',
-                                  connection.read(struct.calcsize('<L')))[0]
-        if not image_len:
-            break
+        # image_len = struct.unpack('<L',
+        #                           connection.read(struct.calcsize('<L')))[0]
+        # if not image_len:
+        #     break
+
         # Construct a stream to hold the image data and read the image
         # data from the connection
         image_stream = io.BytesIO()
         image_stream.write(connection.read(image_len))
+
         # Rewind the stream, open it as an image with PIL and do some
         # processing on it
         image_stream.seek(0)
         image = Image.open(image_stream)
-        print(connection.getpeername())
-        name = '%s.jpg' % (str(datetime.now()))
+        pod_src = connection.getpeername())
+        name = '%s-%s.jpg' % pod_src, (str(datetime.now()))
+
         # outfile = '%s/%s.jpg' % (self.tgtdir,
         # self.basename + str(datetime.now()))
         image.save('/home/pi/NewPod/photo/' + name, 'JPEG')
         # print('Image is %dx%d' % image.size)
         image.verify()
         print('Image is verified')
+
+        connection.close()
+
 finally:
-    connection.close()
+    # connection.close()
     server_socket.close()
--- client/camera_client.py
+++ client/camera_client.py
@@ -23,12 +23,13 @@ client_socket.connect(('192.168.1.100', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
+
 try:
     with picamera.PiCamera() as camera:
+
         camera.resolution = (640, 480)
-        # Start a preview and let the camera warm up for 2 seconds
-        #camera.start_preview()
-        #time.sleep(2)
+        camera.start_preview()
+        time.sleep(2)
 
         # Note the start time and construct a stream to hold image data
         # temporarily (we could write it directly to connection but in this
@@ -42,24 +43,31 @@ try:
 
         cur_time = time.time() - exp_start
         while int(cur_time) < exp_dur:
-            if int(cur_time) % camera_delay == 0:
-                for foo in camera.capture_continuous(stream, 'jpeg'):
-                    # Write the length of the capture to the stream and flush to
-                    # ensure it actually gets sent
-                    connection.write(struct.pack('<L', stream.tell()))
-                    connection.flush()
-                    # Rewind the stream and send the image data over the wire
-                    stream.seek(0)
-                    connection.write(stream.read())
-                    # If we've been capturing for more than 30 seconds, quit
-                    if time.time() - start > 30:
-                        break
-                    # Reset the stream for the next capture
-                    stream.seek(0)
-                    stream.truncate()
-                # Write a length of zero to the stream to signal we're done
-                connection.write(struct.pack('<L', 0))
+
+            # if int(cur_time) % camera_delay == 0:
+
+            camera.capture(stream, 'jpeg')
+
+            # Write the length of the capture to the stream and flush to
+            # ensure it actually gets sent
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+
+            # Rewind the stream and send the image data over the wire
+            stream.seek(0)
+            connection.write(stream.read())
+
+            # Reset the the stream for the next capture
+            stream.seek(0)
+            stream.truncate()
+
+            # Write a length of zero to the stream to signal we're done
+            # connection.write(struct.pack('<L', 0))
+
+            time.sleep(camera_delay)
+
             cur_time = time.time() - exp_start
+
 finally:
     connection.close()
     client_socket.close()
